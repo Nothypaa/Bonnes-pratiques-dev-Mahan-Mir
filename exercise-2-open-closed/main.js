d4ea210d8203aabe143b21b3c42ab7f0f6dffc62
Nothypaa <Mahan.mir@ynov.Com>
2025-11-20 20:54:08 +0100

    main.js

---
+++ exercise-2-open-closed/main.js
@@ -0,0 +1,4 @@
+const amountInput = document.querySelector('#order-amount');
+const shippingSelect = document.querySelector('#shipping-type');
+const calcButton = document.querySelector('#calc-btn');
+const resultEl = document.querySelector('#result');
